a2bd94c9bddd9ed729c74c4c843c88b16896c406
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-04-21 20:16:31 -0500

    Update conversation.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import os
 from dotenv import load_dotenv
 import openai
+from rasa_nlu.training_data import load_data
+from rasa_nlu.config import RasaNLUModelConfig
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+
+training_data = load_data("nlu_data.md")
+trainer = Trainer(config.load("nlu_config.yml"))
+interpreter = trainer.train(training_data)
+
+# Save the trained model for future use
+model_directory = trainer.persist("./models/nlu", fixed_model_name="current")
 
 load_dotenv()
 
@@ -21,8 +32,25 @@ def generate_gpt3_response(prompt):
 
 
 def process_input(user_input):
-    prompt = f"Athena, please help me with the following: {user_input}"
-    response = generate_gpt3_response(prompt)
+    # Use the Rasa NLU interpreter to parse the user input
+    parsed_input = interpreter.parse(user_input)
+
+    # Extract intent and entities from the parsed input
+    intent = parsed_input["intent"]["name"]
+    entities = {entity["entity"]: entity["value"] for entity in parsed_input["entities"]}
+
+    # Process the user input based on the identified intent and entities
+    if intent == "greet":
+        response = "Hello! How can I help you today?"
+    elif intent == "goodbye":
+        response = "Goodbye! Have a great day!"
+    elif intent == "ask_weather":
+        location = entities.get("location", "unknown")
+        response = f"I'm not currently able to check the weather, but you asked about {location}."
+    else:
+        prompt = f"Athena, please help me with the following: {user_input}"
+        response = generate_gpt3_response(prompt)
+
     return response
 
 
---
+++ nlu_data.md
@@ -0,0 +1,14 @@
+## intent:greet
+- hi
+- hello
+- hey
+
+## intent:goodbye
+- bye
+- goodbye
+- see you later
+
+## intent:ask_weather
+- what's the weather like in [New York](location)?
+- how's the weather in [Los Angeles](location)?
+- what is the temperature in [Paris](location)?
