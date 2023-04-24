d47758d5c65c98e3be9224a5f1bd88767ac97edc
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-04-24 00:10:05 -0500

    update pypi commands.

--- publish.bat
+++ publish.bat
@@ -2,5 +2,5 @@ docker-compose build
 docker-compose push
 rm dist -r
 python -m build
-python -m twine upload --repository testpypi dist/
-python -m twine upload --repository pypi dist/
+python -m twine upload --repository pypi dist/*
+python -m twine upload --repository testpypi dist/*
