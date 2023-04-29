8a9cae2f80d40d309ca4244d288014cf9dcec510
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-04-29 16:13:17 -0500

    Add redis

---
+++ athena/memory/redis.py
@@ -0,0 +1,10 @@
+"""Redis memory provider."""
+from __future__ import annotations
+import os
+import redis
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "athena")
+
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
